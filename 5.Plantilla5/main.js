6be05131bcf5535120795af261db26d9605ac556
euclidesseg <euclides2696@gmail.com>
2024-02-09 17:40:41 -0500

    add design of site 5

--- 5.Plantilla5/main.js
+++ 5.Plantilla5/main.js
@@ -1,4 +1,5 @@
 //Configuración de colores generales
+
 tailwind.config = {
   theme: {
     extend: {
@@ -10,6 +11,13 @@ tailwind.config = {
         greenClear: "#a7c382",
         greenDestacados: "#22c55e",
         blueTitle: "#006eb6",
+        greenEnlaceEncuesta: "#76b82a"
+      },
+      height: {
+        "400": "400px"
+      },
+      width: {
+        "400": "400px"
       },
     },
   },
@@ -37,99 +45,6 @@ btnClose.addEventListener("click", () => {
   document.body.style.overflow = "auto";
 });
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     var navbar = document.getElementById('navbar');
-
-//     window.addEventListener('scroll', function () {
-//         // Añadir clase 'fixed' al hacer scroll hacia abajo
-//         if (window.scrollY > 0) {
-//             navbar.classList.add('fixed');
-//         } else {
-//             // Remover clase 'fixed' al volver al inicio
-//             navbar.classList.remove('fixed');
-//         }
-//     });
-// });
-
-var swiperSlider = new Swiper(".sliderHome", {
-  spaceBetween: 50,
-  slidesPerView: 1,
-  navigation: {
-    nextEl: ".swiper-button-next-slider",
-    prevEl: ".swiper-button-prev-slider",
-  },
-});
-
-var swiperIconos = new Swiper(".swiperIconos", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next-iconos",
-    prevEl: ".swiper-button-prev-iconos",
-  },
-  breakpoints: {
-    // when window width is >= 320px
-    0: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-
-    340: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 10,
-    },
-    1024: {
-      slidesPerView: 4,
-      spaceBetween: 30,
-    },
-    1280: {
-      slidesPerView: 5,
-      spaceBetween: 30,
-    },
-  },
-});
-
-var swiperNoticias = new Swiper(".swiperNoticias", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    // when window width is >= 320px
-    0: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-
-    340: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 10,
-    },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-    1280: {
-      slidesPerView: 4,
-      spaceBetween: 30,
-    },
-  },
-});
-
 //Funcion para obtener la fecha del sistema
 
 function obtenerFechaFormateada() {
@@ -150,5 +65,24 @@ function obtenerFechaFormateada() {
   lblDateSystem.innerText = fechaFormateada;
 }
 
+// slider with splide
+document.addEventListener('DOMContentLoaded', function () {
+  var splide = new Splide('.splide', {
+    classes: {
+      arrows: 'splide__arrows your-class-arrows',
+      arrow: 'splide__arrow your-class-arrow',
+      prev: 'splide__arrow--prev your-class-prev bg-white',
+      next: 'splide__arrow--next bg-white',
+    },
+    arrows: {
+      next: "",
+      prev: ""
+    },
+    autoplay: true,
+    type: 'loop',
+  });
+
+  splide.mount();
+});
 // Llamar a la función
 obtenerFechaFormateada();
