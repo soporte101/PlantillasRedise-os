302a4187b9711da1c3d5d67804d144a31e8a3a5d
Juan Pablo Garcia Hernandez <darkcronosyt166@gmail.com>
2024-02-09 15:33:07 -0500

    Primera parte

--- 2.Plantilla2/main.js
+++ 2.Plantilla2/main.js
@@ -10,6 +10,7 @@ tailwind.config = {
         greenClear: "#a7c382",
         greenDestacados: "#22c55e",
         blueTitle: "#006eb6",
+        colorMunicipio: "#138f4c"
       },
     },
   },
@@ -22,17 +23,12 @@ const menu = document.getElementById("menu"),
 btnMenu.addEventListener("click", () => {
   menu.classList.toggle("translate-x-[-100%]");
 
-  btnMenu.classList.toggle("hidden");
-  btnClose.classList.toggle("hidden");
-
   document.body.style.overflow = "hidden";
 });
 
 btnClose.addEventListener("click", () => {
   menu.classList.toggle("translate-x-[-100%]");
 
-  btnMenu.classList.toggle("hidden");
-  btnClose.classList.toggle("hidden");
 
   document.body.style.overflow = "auto";
 });
@@ -51,84 +47,20 @@ btnClose.addEventListener("click", () => {
 //     });
 // });
 
-var swiperSlider = new Swiper(".sliderHome", {
-  spaceBetween: 50,
-  slidesPerView: 1,
-  navigation: {
-    nextEl: ".swiper-button-next-slider",
-    prevEl: ".swiper-button-prev-slider",
+
+var splide = new Splide( '.slider__home', {
+  classes: {
+    arrows: 'splide__arrows',
+    arrow : 'rounded-full',
+    prev  : 'splide__arrow--prev absolute !left-5 top-1/2 transform -translate-y-1/2 scale-x-[1] min-w-[2.2rem] h-[2.2rem] border-0 flex items-center justify-center bg-black bg-opacity-50 shadow-sm focus:!outline-none [&>svg]:w-[1.3rem]',
+    next  : 'splide__arrow--next absolute !right-5 top-1/2 transform -translate-y-1/2 min-w-[2.2rem] h-[2.2rem] border-0 flex items-center justify-center shadow-sm focus:!outline-none [&>svg]:w-[1.3rem] bg-black bg-opacity-50',
+    pagination: 'splide__pagination flex gap-1 right-[29rem] transform translate-x-1/2',
+	  page      : 'splide__pagination__page border-2 border-solid !bg-transparent w-4 h-4 shadow-md',
   },
+  rewind : true,
+  autoplay: 'play',
 });
-
-var swiperIconos = new Swiper(".swiperIconos", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next-iconos",
-    prevEl: ".swiper-button-prev-iconos",
-  },
-  breakpoints: {
-    // when window width is >= 320px
-    0: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-
-    340: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 10,
-    },
-    1024: {
-      slidesPerView: 4,
-      spaceBetween: 30,
-    },
-    1280: {
-      slidesPerView: 5,
-      spaceBetween: 30,
-    },
-  },
-});
-
-var swiperNoticias = new Swiper(".swiperNoticias", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    // when window width is >= 320px
-    0: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-
-    340: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 10,
-    },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-    1280: {
-      slidesPerView: 4,
-      spaceBetween: 30,
-    },
-  },
-});
+splide.mount();
 
 //Funcion para obtener la fecha del sistema
 
