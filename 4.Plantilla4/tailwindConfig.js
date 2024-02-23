44962b2f78610f57b5f7f054c49db9375371446a
esteark <eardila1409@gmail.com>
2024-02-23 17:15:42 -0500

    Add changes Platilla 4

--- 4.Plantilla4/tailwindConfig.js
+++ 4.Plantilla4/tailwindConfig.js
@@ -1,33 +1,32 @@
 //Configuración de colores generales
 
-
 tailwind.config = {
-    theme: {
-      extend: {
-        colors: {
-          gobColor: "#3366cc",
-          gris: "#f2f2f2",
-          grayColor: "#575656",
-          colorFooter: "#0b3b5d",
-          greenClear: "#a7c382",
-          greenDestacados: "#22c55e",
-          blueTitle: "#006eb6",
-          bgServices:"#006091",
-          mainColorTramites:"#36a9e1",
-          secondaryBlue:"#0085c8",
-          mainHoverTramites:"#014883",
-          colorButtonNoticias:"006ba0",
-          colorSeccionNoticias:"c6c6c6",
-        },
-        width:{
-          anchodecirculos:"220px",
-          anchoCirculoInterior:"202px"
-        },
-        height:{
-          altodecirculos: "220px",
-          altoCirculoInterior:"205px"
-  
-        }
+  theme: {
+    extend: {
+      colors: {
+        gobColor: "#3366cc",
+        gris: "#f2f2f2",
+        yellowMain: "#fddd02",
+        grayColor: "#575656",
+        colorFooter: "#0b3b5d",
+        greenClear: "#a7c382",
+        greenDestacados: "#22c55e",
+        blueTitle: "#006eb6",
+        bgServices: "#006091",
+        mainColorTramites: "#36a9e1",
+        secondaryBlue: "#0085c8",
+        mainHoverTramites: "#014883",
+        colorButtonNoticias: "006ba0",
+        colorSeccionNoticias: "c6c6c6",
+      },
+      width: {
+        anchodecirculos: "220px",
+        anchoCirculoInterior: "202px",
+      },
+      height: {
+        altodecirculos: "220px",
+        altoCirculoInterior: "205px",
       },
     },
-  };
+  },
+};
