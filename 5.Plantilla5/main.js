f813dd4424c56f1bb34f0876fadee6317927a6b4
euclidesseg <euclides2696@gmail.com>
2024-02-23 15:33:09 -0500

    template previews 5 added

--- 5.Plantilla5/main.js
+++ 5.Plantilla5/main.js
@@ -9,15 +9,31 @@ tailwind.config = {
         grayColor: "#575656",
         greenFooter: "#107031",
         greenClear: "#a7c382",
+        greenCardServices: "#22682f",
         greenDestacados: "#22c55e",
         blueTitle: "#006eb6",
-        greenEnlaceEncuesta: "#76b82a"
+        lightGreen: "#76b82a",
+        bgSectionServices: "#dcdcdb",
       },
       height: {
-        "400": "400px"
+        "400": "400px",
+        "cardsHeight": "270px",
+        "containerServices": "1235px",
+        "heightNews": "1412px",
+        "heightFirstNew": "700px",
+        "95%": "95%",
       },
       width: {
-        "400": "400px"
+        "cardsWidth": "280px",
+        "400": "400px",
+        "639": "639px",
+        "1250": "1450px",
+        "containerCards": "1280px",
+        "1576": "1576px",
+        "95%": "95%",
+      },
+      spacing: {
+        '20px': '20px',
       },
     },
   },
