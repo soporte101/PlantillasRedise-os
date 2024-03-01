3eb41c2265b3983e18a5085e3459c7dd331d2677
euclidesseg <euclides2696@gmail.com>
2024-03-01 15:09:29 -0500

    Commit with the changes finaly in html an css

--- 5.Plantilla5/main.js
+++ 5.Plantilla5/main.js
@@ -9,11 +9,11 @@ tailwind.config = {
         grayColor: "#575656",
         greenFooter: "#107031",
         greenClear: "#a7c382",
-        greenCardServices: "#22682f",
+        primaryColor: "#22682f",
         greenDestacados: "#22c55e",
         blueTitle: "#006eb6",
-        lightGreen: "#76b82a",
-        bgSectionServices: "#dcdcdb",
+        thirdColor: "#76b82a",
+        secondColor: "#dcdcdb",
       },
       height: {
         "400": "400px",
@@ -38,29 +38,6 @@ tailwind.config = {
     },
   },
 };
-
-const menu = document.getElementById("menu"),
-  btnMenu = document.getElementById("btnMenu"),
-  btnClose = document.getElementById("btnClose");
-
-btnMenu.addEventListener("click", () => {
-  menu.classList.toggle("translate-x-[-100%]");
-
-  btnMenu.classList.toggle("hidden");
-  btnClose.classList.toggle("hidden");
-
-  document.body.style.overflow = "hidden";
-});
-
-btnClose.addEventListener("click", () => {
-  menu.classList.toggle("translate-x-[-100%]");
-
-  btnMenu.classList.toggle("hidden");
-  btnClose.classList.toggle("hidden");
-
-  document.body.style.overflow = "auto";
-});
-
 //Funcion para obtener la fecha del sistema
 
 function obtenerFechaFormateada() {
@@ -86,19 +63,105 @@ document.addEventListener('DOMContentLoaded', function () {
   var splide = new Splide('.splide', {
     classes: {
       arrows: 'splide__arrows your-class-arrows',
-      arrow: 'splide__arrow your-class-arrow',
+      arrow: 'splide__arrow your-class-arrow w-12 h-12 rounded-full',
       prev: 'splide__arrow--prev your-class-prev bg-white',
       next: 'splide__arrow--next bg-white',
+      pagination: 'splide__pagination flex gap-1 right-[29rem] transform translate-x-1/2',
+      page      : 'splide__pagination__page border-2 border-solid bg-primaryColor w-4 h-4 shadow-md',
     },
     arrows: {
       next: "",
       prev: ""
+    },
+    breakpoints: {
+      1920: {
+        perPage: 1,
+        gap: ".5rem",
+      },
+      1440: {
+        perPage: 1,
+        gap: ".5rem",
+      },
+      1280: {
+        perPage: 1,
+      },
+      1024: {
+        perPage: 1,
+        gap: ".5rem",
+      },
+      960: {
+        perPage: 1,
+        gap: ".5rem",
+      },
+      720: {
+        perPage: 1,
+        gap: ".5rem",
+      },
+      640: {
+        perPage: 1,
+        gap: ".5rem",
+      },
+      480: {
+        perPage: 1,
+        gap: ".5rem",
+      },
     },
     autoplay: true,
     type: 'loop',
   });
 
   splide.mount();
+
+  var splide = new Splide( '.patrocinadores',{
+    breakpoints: {
+      1920: {
+        perPage: 6,
+        gap    : '.5rem',
+      },
+      1440: {
+        perPage: 6,
+        gap    : '.5rem',
+      },
+      1280: {
+        perPage: 5,
+      },
+      1024: {
+        perPage: 5,
+        gap    : '.5rem',
+      },
+      960: {
+        perPage: 4,
+        gap    : '.5rem',
+      },
+      720: {
+        perPage: 4,
+        gap    : '.5rem',
+      },
+      640: {
+        perPage: 3,
+        gap    : '.5rem',
+      },
+      480: {
+        perPage: 2,
+        gap    : '.5rem',
+      },
+    },
+    perPage: 4,
+    gap: ".5rem",
+    perMove: 1,
+    autoplay: "play",
+    rewind : true,
+    pagination: false,
+    classes: {
+        arrows: 'splide__arrows',
+        arrow : 'rounded-full',
+        prev  : 'splide__arrow--prev absolute !left-[-3rem] scale-x-[1] top-2 min-w-[2.2rem] h-[2.2rem] border-0 flex items-center justify-center focus:!outline-none [&>svg]:w-[1rem]',
+        next  : 'splide__arrow--next absolute !right-[-3rem] top-2 min-w-[2.2rem] h-[2.2rem] border-0 flex items-center justify-center focus:!outline-none [&>svg]:w-[1rem]',
+    },
+  });
+  
+  splide.mount();
+  
 });
 // Llamar a la función
 obtenerFechaFormateada();
