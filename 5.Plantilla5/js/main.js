fbf4359b08dc8b400185c92a686d9b795d1808d6
euclidesseg <euclides2696@gmail.com>
2024-03-05 16:36:00 -0500

    add changes end

--- 5.Plantilla5/js/main.js
+++ 5.Plantilla5/js/main.js
@@ -1,43 +1,5 @@
-//Configuración de colores generales
 
-tailwind.config = {
-  theme: {
-    extend: {
-      colors: {
-        gobColor: "#3366cc",
-        gris: "#f2f2f2",
-        grayColor: "#575656",
-        greenFooter: "#107031",
-        greenClear: "#a7c382",
-        primaryColor: "#22682f",
-        greenDestacados: "#22c55e",
-        blueTitle: "#006eb6",
-        thirdColor: "#76b82a",
-        secondColor: "#dcdcdb",
-      },
-      height: {
-        "400": "400px",
-        "cardsHeight": "270px",
-        "containerServices": "1235px",
-        "heightNews": "1412px",
-        "heightFirstNew": "700px",
-        "95%": "95%",
-      },
-      width: {
-        "cardsWidth": "280px",
-        "400": "400px",
-        "639": "639px",
-        "1250": "1450px",
-        "containerCards": "1280px",
-        "1576": "1576px",
-        "95%": "95%",
-      },
-      spacing: {
-        '20px': '20px',
-      },
-    },
-  },
-};
+
 //Funcion para obtener la fecha del sistema
 
 function obtenerFechaFormateada() {
