cec1a116d772e4782aa75e9604c0f4e49fea30be
santiago ceballos <santiceballos44@outlook.com>
2024-11-08 17:15:27 -0500

    style(plantilla): :lipstick: actualizacion de diseño PT1
    se actualiza la plantilla 1 con nuevos diseño como en la
    seccion de silider y creacion de redes sociales
    * sliders
    * colores
    * espaciados

---
+++ 1.Plantilla1/tailwind.config.mjs
@@ -0,0 +1,19 @@
+// Configuración de colores generales
+
+module.exports = {
+	content: ['*.{html,js}'],
+	theme: {
+		extend: {
+			colors: {
+				gobColor: '#3366cc',
+				gris: '#f2f2f2',
+				grayColor: '#575656',
+				greenFooter: '#107031',
+				greenClear: '#a7c382',
+				greenDestacados: '#22c55e',
+				blueTitle: '#006eb6',
+			},
+		},
+	},
+	plugins: [],
+};
